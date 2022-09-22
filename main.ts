6ea1176cadc30c1c714b2e4646d651017d546057
hla2291 <113639212+hla2291@users.noreply.github.com>
2022-09-22 16:56:02 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,5 @@
-let beat = 0
 let tone = 0
-basic.forever(function () {
-    basic.showNumber(beat)
-})
+let beat = 0
 basic.forever(function () {
     tone = Math.map(input.temperature(), -5, 50, 200, 800)
     beat = Math.map(input.temperature(), -5, 50, 1000, 62.5)
